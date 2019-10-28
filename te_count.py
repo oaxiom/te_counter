51b2a5eaac788f840b13681614daf99c8e3a57e9
oaxiom <oaxiom@gmail.com>
2019-10-28 12:51:52 +0800

    rearranged SE and PE and implemented SE mode

--- te_count.py
+++ te_count.py
@@ -123,23 +123,85 @@ class measureTE:
     def parse_bamse(self, filename, logger=None):
         '''
         **Purpose**
-            Load in a BAM SE file, ideally output by collect_valid_pairs.py,
-            although I guess any valid BED will do
-
-            This function is not officially part of te_hic, but could be useful to annotate
-            (for example) a BED list of peaks from a ChIP-seq
+            Load in a BAMPE file
 
         **Arguments**
             filename (Required)
-                filename of the BED file
-
-            expand_bed (Optional, default=0)
-                Optionally expand the BED coordianted left and right by expand_bed
-
+                filename of the BAMSE file
         '''
         assert filename, 'You must specify a filename'
 
-        pass
+        final_results = {i: 0 for i in self.all_feature_names}
+
+        bucket_size = glbase3.config.bucket_size
+
+        sam = pysam.AlignmentFile(filename, 'r')
+        idx = 0
+
+        try:
+            while 1:
+                idx += 1
+                read1 = next(sam)
+                if read1.is_unmapped or read1.is_duplicate or read1.is_qcfail:
+                    continue
+
+                chrom = read1.reference_name.replace('chr', '')
+                left = read1.reference_start
+                rite = read1.reference_end
+
+                if chrom not in self.genome.buckets: # Must be a valid chromosome
+                    continue
+
+                # reach into the genelist guts...
+                # work out which of the buckets is required:
+                left_buck = ((left-1)//bucket_size) * bucket_size
+                right_buck = (rite//bucket_size) * bucket_size
+                buckets_reqd = list(range(left_buck, right_buck+bucket_size, bucket_size))
+                result = []
+                # get the ids reqd.
+                loc_ids = set()
+                if buckets_reqd:
+                    loc1 = glbase3.location(chr=chrom, left=left, right=left+1)
+                    loc2 = glbase3.location(chr=chrom, left=rite-1, right=rite) # I just align the two edges, then I don't need to worry about split-reads, and I rely on the duplicate removal
+                    # To get rid of the same gene twice
+                    for buck in buckets_reqd:
+                        if buck in self.genome.buckets[chrom]:
+                            loc_ids.update(self.genome.buckets[chrom][buck]) # set = unique ids
+
+                    for index in loc_ids:
+                        #print loc.qcollide(self.linearData[index]["loc"]), loc, self.linearData[index]["loc"]
+                        if loc1.qcollide(self.genome.linearData[index]["loc"]): # Any 1 bp overlap...
+                            result.append(self.genome.linearData[index])
+
+                        if loc2.qcollide(self.genome.linearData[index]["loc"]): # Any 1 bp overlap...
+                            result.append(self.genome.linearData[index])
+
+                    if result:
+                        # do the annotation so that a read only gets counted to a TE if it does not hit a gene:
+                        #for r in result:
+                        #    print(r)
+                        types = set([i['type'] for i in result])
+                        ensgs = set([i['ensg'] for i in result]) # only count 1 read to 1 gene
+                        if 'protein_coding' in types or 'lincRNA' in types:
+                            for e in ensgs:
+                                if ':' in ensgs: # A TE, skip it
+                                    continue
+                                final_results[e] += 1
+                        elif 'TE' in types:
+                            for e in ensgs: # Not in any other mRNA, so okay to count as a TE
+                                final_results[e] += 1
+                        #print()
+                if idx % 1000000 == 0:
+                    log.info('Processed {:,} reads'.format(idx))
+                    #break
+        except StopIteration:
+            pass # the last read
+
+        sam.close()
+        log.info('Processed {:,} reads'.format(idx))
+        self.total_reads = idx
+
+        return final_results
 
     def save_result(self, result, out_filename, log=None):
         '''
@@ -168,15 +230,15 @@ def prepare_parser():
     description = 'Counts up the number of reads that overlap some set of gene/TE features'
 
     parser = argparse.ArgumentParser(prog='te_count', description=description, epilog=exmp)
+
     # Optional:
     optional = parser._action_groups.pop()
-    optional.add_argument('-e', '--expwhite', nargs=1, required=False, help='A txt file containing the expected whitelist of barcodes to correct the observed barcodes with')
-    optional.add_argument('--pe', action='store_true', required=False, help='Set mode to PE (paried-end) mode, default is single-end mode')
+    optional.add_argument('--se', action='store_true', required=False, help='Set mode to SE (single-end) mode, default is paired-end mode')
 
     required = parser.add_argument_group('required arguments')
 
     required.add_argument('-i', '--inbam', nargs=1, required=True, help='the BAM alignment file containing the reads')
-    required.add_argument('-o', '--outtsv', nargs=1, required=True, help='theTSV file to save the genees and count data to')
+    required.add_argument('-o', '--outtsv', nargs=1, required=True, help='the TSV file to save the genes and count data to')
     required.add_argument('-g', '--genome', nargs=1, required=True, help='A txt file to save the observed barcode whitelist to')
 
     parser._action_groups.append(optional)
@@ -202,7 +264,7 @@ def main():
     log.info('  inbam: %s' % args.inbam[0])
     log.info('  outtsv: %s' % args.outtsv[0])
     log.info('  genome: "%s"' % args.genome[0])
-    log.info('  paired-end: {0}'.format(args.pe))
+    log.info('  single-end mode: {0} (default is PE)'.format(args.se))
 
     log.info('Finding Genome')
     species = args.genome[0]
@@ -215,10 +277,10 @@ def main():
     mte.bind_genome(os.path.join(script_path, 'genome/%s_glb_gencode_tes.glb' % species))
     log.info('Found and loaded {0} genome'.format(args.genome[0]))
 
-    if args.pe:
-        result = mte.parse_bampe(args.inbam[0], log=log)
+    if args.se:
+        result = mte.parse_bamse(args.inbam[0], log=log)
     else:
-        1/0 # Not implemented!!!
+        result = mte.parse_bampe(args.inbam[0], log=log)
 
     mte.save_result(result, args.outtsv[0], log=log)
 
