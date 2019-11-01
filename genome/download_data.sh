1b9282fa6f5e8ee78f987a5f0b5fc7bcaf7a66c9
oaxiom <oaxiom@gmail.com>
2019-11-01 08:10:31 +0800

    update to M23

--- genome/download_data.sh
+++ genome/download_data.sh
@@ -2,8 +2,8 @@
 # May not be correct or latest versions, but checked on 21/1/2019.
 
 # gencode
-wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.annotation.gtf.gz
-wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M20/gencode.vM20.annotation.gtf.gz
+wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.annotation.gtf.gz
+wget -c ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/gencode.vM23.annotation.gtf.gz
 
 wget -c -O hg38_rmsk.txt.gz http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/rmsk.txt.gz
 wget -c -O mm10_rmsk.txt.gz http://hgdownload.soe.ucsc.edu/goldenPath/mm10/database/rmsk.txt.gz
